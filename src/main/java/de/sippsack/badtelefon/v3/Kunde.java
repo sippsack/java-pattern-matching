cefa677520bfdcad5ea7e8a1d42ea0e3b0199840
merlin.boegershausen <merlin.boegershausen@rwth-aachen.de>
2022-03-17 11:40:14 +0100

    Step 8: Refactor for readability

--- src/main/java/de/sippsack/badtelefon/v3/Kunde.java
+++ src/main/java/de/sippsack/badtelefon/v3/Kunde.java
@@ -16,23 +16,29 @@ public class Kunde {
 
     public void account(int minuten, Zeitpunkt zeitpunkt) {
         gesamtGebuer += switch (tarif) {
-            case PrivatTarif privatTarif -> {
-                var factor = (100 - privatTarif.rabatt()) / 100;
-                var minutenPreis = zeitpunkt.isMondschein() ? PrivatTarif.MONDSCHEINPREISPROMINUTE : PrivatTarif.PREISPROMINUTE;
-                var nettoMinuten = privatTarif.getNettoMinuten(minuten);
-                yield factor * nettoMinuten * minutenPreis;
-            }
-            case BusinessTarif businessTarif && businessTarif.isVipKunde() -> {
-                var minutenPreis = zeitpunkt.isMondschein() ? BusinessTarif.MONDSCHEINPREISPROMINUTE : BusinessTarif.PREISPROMINUTE;
-                yield 0.8 * minuten * minutenPreis;
-            }
-            case BusinessTarif ignore -> {
-                var minutenPreis = zeitpunkt.isMondschein() ? BusinessTarif.MONDSCHEINPREISPROMINUTE : BusinessTarif.PREISPROMINUTE;
-                yield minuten * minutenPreis;
-            }
+            case PrivatTarif privatTarif -> berechnePrivatAnruf(privatTarif, zeitpunkt, minuten);
+            case BusinessTarif businessTarif && businessTarif.isVipKunde() -> berechneVIPAnruf(zeitpunkt, minuten);
+            case BusinessTarif ignore -> berechneBusinessAnruf(zeitpunkt, minuten);
             case ProfiTarif ignore -> minuten * ProfiTarif.PREISPROMINUTE;
             case null -> 60;
         };
+    }
+
+    private double berechnePrivatAnruf(PrivatTarif privatTarif, Zeitpunkt zeitpunkt, int minuten) {
+        var factor = (100 - privatTarif.rabatt()) / 100;
+        var minutenPreis = zeitpunkt.isMondschein() ? PrivatTarif.MONDSCHEINPREISPROMINUTE : PrivatTarif.PREISPROMINUTE;
+        var nettoMinuten = privatTarif.getNettoMinuten(minuten);
+        return factor * nettoMinuten * minutenPreis;
+    }
+
+    private double berechneVIPAnruf(Zeitpunkt zeitpunkt, int minuten) {
+        var minutenPreis = zeitpunkt.isMondschein() ? BusinessTarif.MONDSCHEINPREISPROMINUTE : BusinessTarif.PREISPROMINUTE;
+        return 0.8 * minuten * minutenPreis;
+    }
+
+    private double berechneBusinessAnruf(Zeitpunkt zeitpunkt, int minuten) {
+        var minutenPreis = zeitpunkt.isMondschein() ? BusinessTarif.MONDSCHEINPREISPROMINUTE : BusinessTarif.PREISPROMINUTE;
+        return minuten * minutenPreis;
     }
 
     public double getGebuehr() {
