5ce10d397c82ea417df5e309d88d98c6f1908009
merlin.boegershausen <merlin.boegershausen@rwth-aachen.de>
2022-03-17 11:12:22 +0100

    Step 3: Refactor old-style-switch to enhanced switch

--- src/main/java/de/sippsack/badtelefon/v3/tarif/PreisBerechnungsVisitor.java
+++ src/main/java/de/sippsack/badtelefon/v3/tarif/PreisBerechnungsVisitor.java
@@ -12,24 +12,19 @@ public class PreisBerechnungsVisitor<T extends Tarif> implements TarifVisitor<T>
     @Override
     public void visit(T tarif, int minuten, Zeitpunkt zeitpunkt) {
         switch (tarif) {
-            case PrivatTarif privatTarif: {
+            case PrivatTarif privatTarif -> {
                 var factor = (100 - privatTarif.getRabatt()) / 100;
                 var minutenPreis = zeitpunkt.isMondschein() ? PrivatTarif.MONDSCHEINPREISPROMINUTE : PrivatTarif.PREISPROMINUTE;
                 var nettoMinuten = privatTarif.getNettoMinuten(minuten);
                 gebuehr += factor * nettoMinuten * minutenPreis;
-                break;
             }
-            case BusinessTarif businessTarif: {
+            case BusinessTarif businessTarif -> {
                 var factor = businessTarif.isVipKunde() ? 0.8 : 1.0;
                 var minutenPreis = zeitpunkt.isMondschein() ? BusinessTarif.MONDSCHEINPREISPROMINUTE : BusinessTarif.PREISPROMINUTE;
                 gebuehr += factor * minuten * minutenPreis;
-                break;
             }
-            case ProfiTarif ignore:
-                gebuehr += minuten * ProfiTarif.PREISPROMINUTE;
-                break;
-            case null, default:
-                gebuehr += 60;
+            case ProfiTarif ignore -> gebuehr += minuten * ProfiTarif.PREISPROMINUTE;
+            case null, default -> gebuehr += 60;
         }
     }
 }
