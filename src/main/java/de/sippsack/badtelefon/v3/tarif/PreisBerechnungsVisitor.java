09dcedcdf02461d69abb33f9e6d9036c7f497ce3
merlin.boegershausen <merlin.boegershausen@rwth-aachen.de>
2022-03-17 11:06:16 +0100

    Step 1: Introduce Type Pattern in PreisBerechnungsVisitor

--- src/main/java/de/sippsack/badtelefon/v3/tarif/PreisBerechnungsVisitor.java
+++ src/main/java/de/sippsack/badtelefon/v3/tarif/PreisBerechnungsVisitor.java
@@ -11,14 +11,12 @@ public class PreisBerechnungsVisitor<T extends Tarif> implements TarifVisitor<T>
 
     @Override
     public void visit(T tarif, int minuten, Zeitpunkt zeitpunkt) {
-        if (tarif instanceof PrivatTarif) {
-            var privatTarif = (PrivatTarif) tarif;
+        if (tarif instanceof PrivatTarif privatTarif) {
             var factor = (100 - privatTarif.getRabatt()) / 100;
             var minutenPreis = zeitpunkt.isMondschein() ? PrivatTarif.MONDSCHEINPREISPROMINUTE : PrivatTarif.PREISPROMINUTE;
             var nettoMinuten = privatTarif.getNettoMinuten(minuten);
             gebuehr += factor * nettoMinuten * minutenPreis;
-        } else if (tarif instanceof BusinessTarif) {
-            var businessTarif = (BusinessTarif) tarif;
+        } else if (tarif instanceof BusinessTarif businessTarif) {
             var factor = businessTarif.isVipKunde() ? 0.8 : 1.0;
             var minutenPreis = zeitpunkt.isMondschein() ? BusinessTarif.MONDSCHEINPREISPROMINUTE : BusinessTarif.PREISPROMINUTE;
             gebuehr += factor * minuten * minutenPreis;
