0b74ee3ff505c364333e37153d66a6d4ae7f87e0
Falk Sippach <falk@jug-da.de>
2022-10-20 17:00:06 +0200

    switch to Java 19

--- src/main/java/de/sippsack/badtelefon/v2/tarif/TarifVisitor.java
+++
@@ -1,7 +0,0 @@
-package de.sippsack.badtelefon.v2.tarif;
-
-import de.sippsack.badtelefon.v2.Zeitpunkt;
-
-public interface TarifVisitor<T extends Tarif> {
-    void visit(T tarif, int minuten, Zeitpunkt zeitpunkt);
-}
